a8b27243b835be280e8a909095165e52507a696a
Crowdbotics <team@crowdbotics.com>
2024-01-17 20:24:49 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quick-demo-myvodafo-45412",
+  url: "https://quick-demo-myvodafo-45412.botics.co",
+  api: "https://quick-demo-myvodafo-45412.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
